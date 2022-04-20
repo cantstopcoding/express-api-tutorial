09601de4da0ebb54a7faa838edd1a6e335e345ec
cantstopcoding <jonathan_koss@yahoo.com>
2022-04-20 14:22:28 -0400

    implement mongoose

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
+mongoose.connect("http://localhost:3000", () => {
+  console.log("connected to database");
+});
+
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
