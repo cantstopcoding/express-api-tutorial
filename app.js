e112dddfd573e1232f77a9855899ac01c2e70efa
cantstopcoding <jonathan_koss@yahoo.com>
2022-04-20 18:48:14 -0400

    use body parser

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // app.js
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 
